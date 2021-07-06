ece2cddb3fc6f44335b22bfdd0b0aa71aea3dee9
tryashtar <tryashtar@gmail.com>
2021-07-05 20:05:59 -0600

    Expose special case doubles/floats separately
    This fixes stuff like "005" getting parsed as a double instead of string

--- DataUtils.cs
+++ DataUtils.cs
@@ -9,42 +9,41 @@ namespace TryashtarUtils.Utility
 {
     public static class DataUtils
     {
+        private static readonly Dictionary<string, (double d, float f)> SpecialFloatingPoints = new(StringComparer.OrdinalIgnoreCase)
+        {
+            { "∞", (double.PositiveInfinity, float.PositiveInfinity) },
+            { "+∞", (double.PositiveInfinity, float.PositiveInfinity) },
+            { "-∞", (double.NegativeInfinity, float.NegativeInfinity) },
+            { "Infinity", (double.PositiveInfinity, float.PositiveInfinity) },
+            { "+Infinity", (double.PositiveInfinity, float.PositiveInfinity) },
+            { "-Infinity", (double.NegativeInfinity, float.NegativeInfinity) },
+            { "NaN", (double.NaN, float.NaN) }
+        };
+
+        public static double? TryParseSpecialDouble(string value)
+        {
+            if (SpecialFloatingPoints.TryGetValue(value, out var result))
+                return result.d;
+            return null;
+        }
+
+        public static float? TryParseSpecialFloat(string value)
+        {
+            if (SpecialFloatingPoints.TryGetValue(value, out var result))
+                return result.f;
+            return null;
+        }
+
         public static double ParseDouble(string value)
         {
-            if (value.Equals("∞", StringComparison.OrdinalIgnoreCase))
-                return double.PositiveInfinity;
-            if (value.Equals("+∞", StringComparison.OrdinalIgnoreCase))
-                return double.PositiveInfinity;
-            if (value.Equals("-∞", StringComparison.OrdinalIgnoreCase))
-                return double.NegativeInfinity;
-            if (value.Equals("Infinity", StringComparison.OrdinalIgnoreCase))
-                return double.PositiveInfinity;
-            if (value.Equals("+Infinity", StringComparison.OrdinalIgnoreCase))
-                return double.PositiveInfinity;
-            if (value.Equals("-Infinity", StringComparison.OrdinalIgnoreCase))
-                return double.NegativeInfinity;
-            if (value.Equals("NaN", StringComparison.OrdinalIgnoreCase))
-                return double.NaN;
-            return double.Parse(value, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+            return TryParseSpecialDouble(value) ??
+                double.Parse(value, NumberStyles.Float, CultureInfo.InvariantCulture);
         }
 
         public static float ParseFloat(string value)
         {
-            if (value.Equals("∞", StringComparison.OrdinalIgnoreCase))
-                return float.PositiveInfinity;
-            if (value.Equals("+∞", StringComparison.OrdinalIgnoreCase))
-                return float.PositiveInfinity;
-            if (value.Equals("-∞", StringComparison.OrdinalIgnoreCase))
-                return float.NegativeInfinity;
-            if (value.Equals("Infinity", StringComparison.OrdinalIgnoreCase))
-                return float.PositiveInfinity;
-            if (value.Equals("+Infinity", StringComparison.OrdinalIgnoreCase))
-                return float.PositiveInfinity;
-            if (value.Equals("-Infinity", StringComparison.OrdinalIgnoreCase))
-                return float.NegativeInfinity;
-            if (value.Equals("NaN", StringComparison.OrdinalIgnoreCase))
-                return float.NaN;
-            return float.Parse(value, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture);
+            return TryParseSpecialFloat(value) ??
+                float.Parse(value, NumberStyles.Float, CultureInfo.InvariantCulture);
         }
 
         public static string DoubleToString(double d)
