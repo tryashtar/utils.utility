bbd4c79ce5c82cb8c22c9636c948f0f3555bbb64
tryashtar <tryashtar@gmail.com>
2022-04-02 20:08:53 -0600

    Case converters

--- StringUtils.cs
+++ StringUtils.cs
@@ -79,10 +79,34 @@ namespace TryashtarUtils.Utility
 
         public static T ParseUnderscoredEnum<T>(string str) where T : struct
         {
-            str = str.ToLower().Replace('_', ' ');
+            return Enum.Parse<T>(SnakeToPascal(str));
+        }
+
+        public static string PascalToSnake(string text)
+        {
+            if (text.Length < 2)
+                return text;
+            var sb = new StringBuilder();
+            sb.Append(char.ToLowerInvariant(text[0]));
+            for (int i = 1; i < text.Length; ++i)
+            {
+                char c = text[i];
+                if (char.IsUpper(c))
+                {
+                    sb.Append('_');
+                    sb.Append(char.ToLowerInvariant(c));
+                }
+                else
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public static string SnakeToPascal(string text)
+        {
+            text = text.ToLower().Replace('_', ' ');
             var info = CultureInfo.CurrentCulture.TextInfo;
-            str = info.ToTitleCase(str).Replace(" ", String.Empty);
-            return Enum.Parse<T>(str);
+            return info.ToTitleCase(text).Replace(" ", String.Empty);
         }
     }
 }
